cf3a68772afbcf16cc51bea5e27e181c499a5091
Dillon Fletcher <32619629+appledeej@users.noreply.github.com>
2019-01-22 18:18:39 +0000

    Add files via upload

---
+++ multifunction.py
@@ -0,0 +1,34 @@
+import pygame
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+
+print("Welcome to appledeej's multi-function sense hat sensory doodle")
+while True:
+    print("1.Temperature")
+    print("2.Humidity")
+    print("3.Barometric Pressure")
+    option = input("Please choose an option: ")
+
+        
+
+    if option == 1:
+        sense = SenseHat()
+        temp = sense.get_temperature()
+        print("Temperature: %s C" % temp)
+        temp = round(temp) 
+        temp = str(temp)
+        sense.show_message(temp,text_colour=[255, 0, 0]) 
+
+    if option == 2:
+        humidity = sense.get_humidity()
+        print("Humidity: %s %%rH" % humidity)
+        humidity = str(humidity)
+        sense.show_message(humidity,text_colour=[255, 0, 0])
+        
+    if option == 3:
+        pressure = sense.get_pressure()
+        print("Pressure: %s Millibars" % pressure)
+        pressure = str(pressure)
+        sense.show_message(pressure,text_colour=[255, 0, 0])
